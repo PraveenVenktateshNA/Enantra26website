5823c8b476183225df22cbffc97287a82112bb93
PraveenVenktateshNA <napraveenvenkatesh@gmail.com>
2026-02-20 01:48:50 +0530

    added new posters

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -6,10 +6,13 @@ import genzBazaarPoster from '../assets/posters/genz_bazaar.jpeg';
 import arenaPoster from '../assets/posters/arenahunt.jpeg';
 import auctionPoster from '../assets/posters/auctionhouse.png';
 import rebootPoster from '../assets/posters/Reboot.jpeg';
-import stockPoster from '../assets/posters/stock_workshop.jpeg';
-import bussPoster from '../assets/posters/business.jpeg';
+import stockPoster from '../assets/posters/stock_Market.png';
+import bussPoster from '../assets/posters/Business_Mastery.png';
 import question from '../assets/posters/20Questions.jpg';
 import doorPoster from '../assets/posters/Doordarshan.png';
+import entrePoster from '../assets/posters/keys.png';
+import networkPoster from '../assets/posters/Networking.png';
+
 export default function Events() {
   const headerRef = useScrollReveal({ threshold: 0.4 });
   const [selectedEvent, setSelectedEvent] = useState(null);
@@ -55,10 +58,10 @@ export default function Events() {
       rules: [ 
               'Induvidual Participation'
             ],
-      dateTime: 'February 24, 2025 | Forenoon',
-      venue: 'Vivek Auditorium',
+      dateTime: 'February 24, 2025 | 10:30 to 12:30 PM',
+      venue: 'Turing Hall',
       contact: 'Swarup | +91 97890 86940 | Thithika | +91 73393 42806',
-      registerLink: 'https://unstop.com/event3'
+      registerLink: 'https://unstop.com/o/hUY8RAB?lb=oDYHKpG&utm_medium=Share&utm_source=workshops&utm_campaign=Abhijvar1195'
     },
     { 
       id: 4,
@@ -160,8 +163,8 @@ export default function Events() {
       rules: [
               'Induvidual Participation'
       ],
-      dateTime: 'February 23, 2025 | Forenoon',
-      venue: 'Computer Center, Building H',
+      dateTime: 'February 23, 2025 | 1:30 to 3:00 PM',
+      venue: 'Hall of Guines',
       contact: 'Bhavatharini | +91 99627 00799 | Thithika | +91 73393 42806',
       registerLink: 'https://unstop.com/p/stock-market-workshop-enantra-100-anna-university-student-entrepreneurship-club-1642869?lb=eUxCbbeV&utm_medium=Share&utm_source=ausec2023&utm_campaign=Workshops'
     },
@@ -169,13 +172,13 @@ export default function Events() {
       id: 11,
       title: 'Networking and Co-Elevation', 
       text: 'An engaging workshop that empowers students to build authentic professional networks and unlock career opportunities.',
-      image: 'https://placehold.co/800x600/000000/FFFFFF/png?text=EVENT+9',
+      image: networkPoster,
       fullDescription: 'Networking for Success is an interactive workshop that helps students build strong professional relationships for career and personal growth. Participants learn effective communication, personal branding, and strategic networking techniques—including leveraging LinkedIn, engaging at professional events, and maintaining meaningful connections. Through practical tips and real-world insights, the session equips students with the confidence to create authentic networks that open doors to internships, jobs, and collaborations.',
       rules: [
               'Induvidual Participation'
       ],
-      dateTime: 'February 24, 2025 | Afternoon',
-      venue: 'Computer Center, Building H',
+      dateTime: 'February 24, 2025 | 1:30 to 3:00 PM',
+      venue: 'Hall of Guines',
       contact: 'Tanish | +91 72002 39402 | Ragavendran S | +91 96776 56128 ',
       registerLink: 'https://unstop.com/p/networking-and-co-elevation-enantra-100-anna-university-student-entrepreneurship-club-1642866?lb=eUxCbbeV&utm_medium=Share&utm_source=ausec2023&utm_campaign=Workshops'
     },
@@ -183,13 +186,13 @@ export default function Events() {
       id: 12,
       title: 'Keys to Entrepreneurial Success', 
       text: 'An inspiring seminar that empowers students to transform ideas into sustainable and successful businesses.',
-      image: 'https://placehold.co/800x600/000000/FFFFFF/png?text=EVENT+9',
+      image: entrePoster,
       fullDescription: 'Entrepreneurship Success Seminar is an interactive workshop that equips students with the mindset and practical skills to turn ideas into successful businesses. Participants learn how to identify opportunities, develop innovative concepts, and understand the fundamentals of business planning, funding, and marketing. Through real-world success stories and actionable insights, the session builds confidence to navigate challenges and create sustainable, growth-driven ventures.',
       rules: [
               'Induvidual Participation'
       ],
-      dateTime: 'February 23, 2025 | Afternoon',
-      venue: 'Computer Center, Building H',
+      dateTime: 'February 23, 2025 | 10:30 to 12:30 PM',
+      venue: 'Hall of Guines',
       contact: 'Tanisha | +91 72002 39402 | Sam | +91 99404 29443',
       registerLink: 'https://unstop.com/p/keys-to-entrepreneurial-success-enantra-100-anna-university-au-chennai-1642856?lb=eUxCbbeV&utm_medium=Share&utm_source=ausec2023&utm_campaign=Workshops'
     }
